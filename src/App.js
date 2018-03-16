1af50674f92141a564fc348740b4d71414286c31
minmaxdata <mcdreamer60@gmail.com>
2018-03-16 15:38:21 -0700

    cleaning up formatting mess

--- src/App.js
+++ src/App.js
@@ -356,50 +356,38 @@ class BooksApp extends React.Component {
     }
 
     render() {
-        return ( <
-            div className = "app" > {
-                this.state.showSearchPage ? ( <
-                    SearchBooks / >
-                ) : ( <
-                    div className = "list-books" >
-                    <
-                    div className = "list-books-title" >
-                    <
-                    h1 > MyReads < /h1> <
-                    /div> <
-                    div className = "list-books-content" >
-                    <
-                    div >
-                    <
-                    div className = "bookshelf" >
-                    <
-                    h2 className = "bookshelf-title" > Currently Reading < /h2>   <
-                    ListBookShelf books = { current }
-                    /> <
-                    /div> <
-                    div className = "bookshelf" >
-                    <
-                    h2 className = "bookshelf-title" > Want to Read < /h2> <
-                    ListBookShelf books = { want }
-                    /> <
-                    /div> <
-                    div className = "bookshelf" >
-                    <
-                    h2 className = "bookshelf-title" > Read < /h2> <
-                    ListBookShelf books = { read }
-                    /> <
-                    /div> <
-                    /div> <
-                    /div> <
-                    div className = "open-search" >
-                    <
-                    a onClick = {
-                        () => this.setState({ showSearchPage: true }) } > Add a book < /a> <
-                    /div> <
-                    /div>
+        return ( 
+            <div className="app" > 
+            { this.state.showSearchPage ? ( 
+                    <SearchBooks / >
+                ) : ( 
+                <div className="list-books">
+                    <div className="list-books-title">
+                        <h1> MyReads </h1> 
+                    </div> 
+                    <div className="list-books-content">
+                        <div>
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title"> Currently Reading < /h2>   
+                                <ListBookShelf books={ current }/> 
+                            </div> 
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title"> Want to Read < /h2> 
+                                <ListBookShelf books={want}/> 
+                            </div> 
+                            <div className="bookshelf" >
+                                <h2 className="bookshelf-title" > Read < /h2> 
+                                <ListBookShelf books={read}/> 
+                            </div> 
+                        </div> 
+                    </div> 
+                    <div className="open-search" >
+                        <a onClick={() => this.setState({ showSearchPage: true }) } > Add a book < /a> 
+                    </div> 
+                </div>
                 )
-            } <
-            /div>
+            } 
+            </div>
         )
     }
 }
--- src/ListBookShelf.js
+++ src/ListBookShelf.js
@@ -2,41 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function ListBookShelf(props) {
-    return ( <
-        div className = "bookshelf-books" >
-        <
-        ol className = "books-grid" > {
-            props.books.map((book) => ( <
-                li key = { book.id } >
-                <
-                div className = "book" >
-                <
-                div className = "book-top" >
-                <
-                div className = "book-cover"
-                style = { { width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` } } > < /div> <
-                div className = "book-shelf-changer" >
-                <
-                select onChange = {
-                    () => props.onChangeShelf(book) } >
-                <
-                option value = "none"
-                disabled > Move to... < /option> <
-                option value = "currentlyReading" > Currently Reading < /option> <
-                option value = "wantToRead" > Want to Read < /option> <
-                option value = "read" > Read < /option> <
-                option value = "none" > None < /option> <
-                /select> <
-                /div> <
-                /div> <
-                div className = "book-title" > { book.title } < /div> <
-                div className = "book-authors" > { book.authors } < /div> <
-                /div> <
-                /li>
+    return ( 
+      <div className="bookshelf-books">
+        <ol className="books-grid"> 
+        { props.books.map((book) => ( 
+              <li key={ book.id }>
+                <div className="book">
+                <div className="book-top">
+                <div className="book-cover" style={ { width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` } }> </div> 
+                <div className="book-shelf-changer">
+                  <select 
+                    onChange={() => props.onChangeShelf(book) } 
+                    value='foo'>
+                    <option value="none" disabled> Move to... </option> 
+
+                    <option value="currentlyReading"> Currently Reading </option> 
+                    <option value="wantToRead"> Want to Read </option>
+                    <option value="read"> Read </option> 
+                    <option value="none"> None </option> 
+                  </select> 
+                </div> 
+                </div> 
+                <div className="book-title"> { book.title } </div> 
+                <div className="book-authors">  { book.authors } </div> 
+                </div> 
+              </li>
             ))
-        } <
-        /ol> <
-        /div>
+        } 
+        </ol> 
+        </div>
     )
 }
 
