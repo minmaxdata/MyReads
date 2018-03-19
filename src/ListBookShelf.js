1ed6ed91c69a35dcb18ba713bdbf9e8af0057561
minmaxdata <mcdreamer60@gmail.com>
2018-03-19 16:52:10 -0700

    more edits to fulfill the requirements for this project

--- src/ListBookShelf.js
+++ src/ListBookShelf.js
@@ -1,43 +1,35 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import Book from './Book'
 
-function ListBookShelf(props) {
-    return ( 
-      <div className="bookshelf-books">
-        <ol className="books-grid"> 
-        { props.books.map((book) => ( 
-              <li key={ book.id }>
-                <div className="book">
-                <div className="book-top">
-                <div className="book-cover" style={ { width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` } }> </div> 
-                <div className="book-shelf-changer">
-                  <select 
-                    onChange={() => props.onChangeShelf(book) } 
-                    value='foo'>
-                    <option value="none" disabled> Move to... </option> 
+class ListBookShelf extends React.Component {
+ 
+  render () {
+    const shelf = this.props.shelf
+    const books = this.props.books
+    const filteredBooks = books.filter( item => item.shelf.toLowerCase() === shelf.replace(/\s/g, '').toLowerCase())
+             
+           
+    return (
+        <div className="bookshelf">
+          <h2 className="bookshelf-title"> {shelf} </h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {filteredBooks.map(book => {
+              return (
 
-                    <option value="currentlyReading"> Currently Reading </option> 
-                    <option value="wantToRead"> Want to Read </option>
-                    <option value="read"> Read </option> 
-                    <option value="none"> None </option> 
-                  </select> 
-                </div> 
-                </div> 
-                <div className="book-title"> { book.title } </div> 
-                <div className="book-authors">  { book.authors } </div> 
-                </div> 
-              </li>
-            ))
-        } 
-        </ol> 
-        </div>
-    )
-}
-
-ListBookShelf.PropTypes = {
-    books: PropTypes.array.isRequired,
-    onChangeShelf: PropTypes.func.isRequired
-
+                <Book
+                  book={book}
+                  shelf={shelf}
+                  key={book.id}
+                  onChangeShelf={this.props.onChangeShelf}
+                />
+              )}
+            )}
+            </ol>
+          </div>
+        </div> 
+      )
+   } 
 }
 
 export default ListBookShelf
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,29 +1,62 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends Component {
+  state = {
+    query: '',
+    books: []
+  }
+
+  updateQuery = (query) => {  
+    this.setState({ query: query.trim() })
+  }
+  searchBook = (query) => {
+    this.updateQuery(query);
+    if (query) {
+      BooksAPI.search(query).then((books) => {
+        if (!books.error) {
+           this.setState({
+            books
+          })
+        } else {
+          this.setState({books: []});
+        }
+      })
+    } else {
+      this.setState({books: []});
+    }
+  }
 
 render () {
 	return (
 		<div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input 
+              type="text" 
+              placeholder="Search by title or author"
+              onChange={(event) => this.searchBook(event.target.value)}
+            />
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
           </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+          {this.state.books.map(book => (
+              <Book
+                book={book}
+                key={book.id}
+                shelf={book.shelf}
+                onChangeShelf={this.props.onChangeShelf}
+              />
+            ))}
+
+          </ol>
+        </div>
+      </div>
 		)
 	
 	}
